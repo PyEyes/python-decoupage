66744d41a23fe7006d207ddd4c0233c03c7a571d
Yohan Gouzerh <Yohan.Gouzerh@grenoble-inp.og>
2016-12-09 19:03:54 +0100

    Commencement du parsing

--- main.py
+++ main.py
@@ -4,15 +4,28 @@
     Fichier principal du projet decoupage.py
 """
 
-from parser_stl import  ParseurStl
+from parser_stl import ParseurStl
+
+import argparse
+
+PARSER_GENERAL = argparse.ArgumentParser()
+PARSER_GENERAL.add_argument('--chemin', type=str)
+ARGUMENTS = PARSER_GENERAL.parse_args()
+
+def test_lecture_rapide_stl():
+    """
+        Lecture rapide du contenu binaire du fichier stl
+    """
+    chemin_fichier_stl = ARGUMENTS.chemin
+    parseur = ParseurStl(chemin_fichier_stl)
+    print(parseur.contenu)
 
 def main():
     """
         Fonction principale qui lance les modules
     """
-    chemin_fichier_stl = ""
-    parseur = ParseurStl(chemin_fichier_stl)
-    parseur.lire_stl()
+    # print("Hello World !")
+    test_lecture_rapide_stl()
 
 if __name__ == '__main__':
     main()
--- parser_stl.py
+++ parser_stl.py
@@ -4,12 +4,16 @@
     Module de parsing pour les fichiers stl
 """
 
+from struct import unpack
+
 class ParseurStl:
     """
         Ouvre le fichier stl et le convertit en objets
     """
     def __init__(self, p_chemin_fichier_stl):
         self.chemin_fichier_stl = p_chemin_fichier_stl
+        with open(self.chemin_fichier_stl, "rb") as stl:
+            self.contenu = unpack("<IIII", stl.read())
 
     def lire_stl(self):
         """
